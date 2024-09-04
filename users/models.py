36be73760d9192ab5fe9076a6f10ecf5b06ba646
Olloyor Makhammatnabiyev <muhammadnabiyevolloyor@gmail.com>
2024-09-04 19:13:21 +0500

    set up check birth date

--- users/models.py
+++ users/models.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core import validators
 from users.errors import BIRTH_YEAR_ERROR_MSG
+from django.contrib.postgres.indexes import HashIndex
+
 # Create your models here.
 
 def file_upload(instance, filename):
@@ -28,17 +30,26 @@ class CustomUser(AbstractUser):
 		    )
     
     class Meta:
-        db_table = "user" #db table name
-        verbose_name = "User"
-        verbose_name_plural = "Users"
-        ordering = ["-date_joined"]
-        constraints = [
-                    models.CheckConstraint(  # tug'ilgan yil oralig'ini tekshirish uchun uchunchi variant
-                        check=models.Q(birth_year__gt=settings.BIRTH_YEAR_MIN) & models.Q(
-                            birth_year__lt=settings.BIRTH_YEAR_MAX),
-                        name='check_birth_year_range'
-                    )
-                ]
+            db_table = "user"
+            verbose_name = "User"
+            verbose_name_plural = "Users"
+            ordering = ["-date_joined"]
+            
+            # Composite Index va Hash Index qo'shish
+            indexes = [
+                HashIndex(fields=['first_name'], name='%(class)s_first_name_hash_idx'),
+                HashIndex(fields=['last_name'], name='%(class)s_last_name_hash_idx'),
+                HashIndex(fields=['middle_name'], name='%(class)s_middle_name_hash_idx'),
+                models.Index(fields=['username'], name='%(class)s_username_idx'),
+            ]
+            
+            constraints = [
+                models.CheckConstraint(
+                    check=models.Q(birth_year__gt=settings.BIRTH_YEAR_MIN) & models.Q(
+                        birth_year__lt=settings.BIRTH_YEAR_MAX),
+                    name='check_birth_year_range'
+                )
+            ]
         
     def clean(self):  # tug'ilgan yil oralig'ini tekshirish uchun ikkinchi variant
         super().clean()
